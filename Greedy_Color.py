2a79569ae5bc5d2c02f1b289cb2abe68aadbaa0b
Jane Lee <jklee@ucdavis.edu>
2017-05-06 00:45:36 -0700

    Commented basic steps, wanted to double check logic with you first

--- Greedy_Color.py
+++ Greedy_Color.py
@@ -9,20 +9,42 @@ os.chdir(os.path.relpath('Stims', start='.')+'\\')
 files=csv.DictReader(open('Stims Similarities.csv','r+'))
 files=list(files)
 
+
+# format dictionary => all stim names in one list called [stims]
+
+# create empty list called [finalStims] 
+
+# for number of elements in [finalStims] < 4, do
+#    choose random row in dictionary 
+#    place stim name in [finalStims]
+#    compare strings under [SIMILAR IN COLOR] and [SIMILAR IN SHAPE] with [stims]
+#       if matches, then
+#          remove from [stims]
+#          remove those stims' rows from dictionary
+#       end if
+# end for
+
+# [final stims] should now be a list of 4 dissimilar fractals
+
+
+
+--------------------------------------------------------------------------------
+
+
 #letters
-letters=list(string.ascii_lowercase)
+#letters=list(string.ascii_lowercase)
 
 
-for i in files[0:5]:
-    #update our dicts to have neighbor entry
-    i.update({'MatchingLetter':[]})
+#for i in files[0:5]:
+#    #update our dicts to have neighbor entry
+#    i.update({'MatchingLetter':[]})
     
-    if files.index(i)==0:
-        i['MatchingLetter']=letters[0]
-    else:
+#    if files.index(i)==0:
+#        i['MatchingLetter']=letters[0]
+#    else:
         #look through all the files
-        for l in files:
-            if l['STIMULUS NAME'] in i['SIMILAR IN COLOR'] or l['STIMULUS NAME'] in i['SIMILAR IN SHAPE']:
-                i['MatchingLetter'].append(l['STIMULUS NAME'])
+#        for l in files:
+#            if l['STIMULUS NAME'] in i['SIMILAR IN COLOR'] or l['STIMULUS NAME'] in i['SIMILAR IN SHAPE']:
+#                i['MatchingLetter'].append(l['STIMULUS NAME'])
             
             
