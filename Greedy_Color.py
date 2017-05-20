ed53a517532b0eb691ee28bbbcfd1701d719365e
Jane Lee <jklee@ucdavis.edu>
2017-05-20 11:41:18 -0700

    Should be fully functional now

--- Greedy_Color.py
+++ Greedy_Color.py
@@ -1,73 +1,61 @@
+#!/usr/bin/python
 import csv, os
 import string
+import random
 string.ascii_lowercase
 
 #point to a relative path
-os.chdir(os.path.relpath('Stims', start='.')+'\\')
+#os.chdir(os.path.relpath('Stims', start='.')+'//')
 
-files=csv.DictReader(open('Stims Similarities.csv','r+'))
+def searchList( object, myList ):
+    for index in range(len(myList)):
+        if object == myList[index]['STIMULUS NAME']:
+            return index;
+
+    return -1;
+
+'''might need to put a full path before file name'''
+files=csv.DictReader(open(os.getcwd()+'/Stims_Similarities.csv','r+'))
 files=list(files)
 
-# format dictionary => all stim names in one list called [stims], shuffle
-stims = [files['STIMULUS NAME']]
-random.shuffle(stims)
-
-# insert first element of [stims] in [finalStims]
-finalStims = []
-finalStims.append(stims[0])
-stims.remove(stims[0])
-
-# remove strings under [SIMILAR IN COLOR] and [SIMILAR IN SHAPE] from [stims]
-if ['SIMILAR IN COLOR'] in [stims]: 
-    stims.remove(['SIMILAR IN COLOR'])
-if ['SIMILAR IN SHAPE'] in [stims]: 
-    stims.remove(['SIMILAR IN SHAPE'])
+for i in files:
+    i['SIMILAR IN COLOR'] = i['SIMILAR IN COLOR'].replace(" ", "")
+    i['SIMILAR IN COLOR'] = i['SIMILAR IN COLOR'].split(',')
+    i['SIMILAR IN SHAPE'] = i['SIMILAR IN SHAPE'].replace(" ", "")
+    i['SIMILAR IN SHAPE'] = i['SIMILAR IN SHAPE'].split(',')
+    i['STIMULUS NAME'] = i['STIMULUS NAME'].replace(" ", "")
+    i['SIMILAR IN COLOR'] = set(i['SIMILAR IN COLOR'])
+    i['SIMILAR IN SHAPE'] = set(i['SIMILAR IN SHAPE'])
     
-# remove those stims' rows from dictionary
-for key in (['SIMILAR IN COLOR'] or ['SIMILAR IN SHAPE']):
-    del files[key] # will this delete the whole dict? Needs to delete the entire dicts corresponding to those keys.
-    
-# for number of elements in [finalStims] < 4, do
-for i in xrange(1,4):
-#    shuffle [stims]
+stims = [file['STIMULUS NAME'] for file in files] # format dictionary => all stim names in one list called [stims], shuffle
+finalStims = [] # initialize list for finalStims
+
+#keep adding items until you get a list of 4, if you have deleted all items from stim, reset
+while(len(finalStims) != 4):
+    #shuffle files and add item from the first index
     random.shuffle(stims)
-#    place stim name in [finalStims]    
     finalStims.append(stims[0])
-    stims.remove(stims[0])
-#    compare strings under [SIMILAR IN COLOR] and [SIMILAR IN SHAPE] with [stims]
-#       if matches, then
-#          remove from [stims]
-    if ['SIMILAR IN COLOR'] in [stims]: 
-        stims.remove(['SIMILAR IN COLOR'])
-    if ['SIMILAR IN SHAPE'] in [stims]:
-        stims.remove(['SIMILAR IN SHAPE'])
-#          remove those stims' rows from dictionary
-    for key in (list('SIMILAR IN COLOR') or list('SIMILAR IN SHAPE')):
-        del files[key]
-#          shuffle stims
-        random.shuffle(stims)
+    tempIndex = searchList(stims[0],files);
 
-# return [final stims], which should now be a list of 4 dissimilar fractals
-print finalStims; 
+    if tempIndex == -1:
+        finalStims = []
+        stims = [file['STIMULUS NAME'] for file in files]
+        continue
 
+    #removes simlarities between the two and the item
+    stims = set(stims) - files[tempIndex]['SIMILAR IN COLOR']
+    stims = stims - files[tempIndex]['SIMILAR IN SHAPE']
 
-#--------------------------------------------------------------------------------
+    #print set(stims).symmetric_difference(files[tempIndex]['SIMILAR IN COLOR'])
+    #print stims.symmetric_difference(files[tempIndex]['SIMILAR IN SHAPE'])
 
+    if files[tempIndex]['STIMULUS NAME'] in stims:
+        stims.remove(files[tempIndex]['STIMULUS NAME'])
+    stims = list(stims) # turn it back into a list
 
-#letters
-#letters=list(string.ascii_lowercase)
+    if len(stims) == 0:
+        finalStims = []
+        stims = [file['STIMULUS NAME'] for file in files]
+        continue
 
-
-#for i in files[0:5]:
-#    #update our dicts to have neighbor entry
-#    i.update({'MatchingLetter':[]})
-    
-#    if files.index(i)==0:
-#        i['MatchingLetter']=letters[0]
-#    else:
-        #look through all the files
-#        for l in files:
-#            if l['STIMULUS NAME'] in i['SIMILAR IN COLOR'] or l['STIMULUS NAME'] in i['SIMILAR IN SHAPE']:
-#                i['MatchingLetter'].append(l['STIMULUS NAME'])
-            
-            
+print finalStims
